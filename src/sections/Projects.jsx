7e63caf82680e6bf2becbe2e42196e44b6a2cabf
JLTDigital <mrjltaft@gmail.com>
2024-10-26 12:22:11 +0100

    update

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,24 +1,17 @@
-import React, { Suspense, useState } from 'react'
-import { myProjects } from '../constants'
-import { calculateSizes } from '../constants'
-import { useMediaQuery } from 'react-responsive'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Center, OrbitControls } from '@react-three/drei'
-import CanvasLoader from '../components/CanvasLoader'
-import DemoComputer from '../components/DemoComputer'
+
+import { myProjects } from '../constants/index.js'
+import CanvasLoader from '../components/CanvasLoader.jsx'
+import DemoComputer from '../components/DemoComputer.jsx'
 
 const projectCount = myProjects.length
 
 const Projects = () => {
-  const isSmall = useMediaQuery({ maxWidth: 440 })
-  const isMobile = useMediaQuery({ maxWidth: 768 })
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
-
-  const sizes = calculateSizes(isSmall, isMobile, isTablet)
-
   const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
-
-  const currentProject = myProjects[selectedProjectIndex]
 
   const handleNavigation = (direction) => {
     setSelectedProjectIndex((prevIndex) => {
@@ -30,13 +23,23 @@ const Projects = () => {
     })
   }
 
+  useGSAP(() => {
+    gsap.fromTo(
+      `.animatedText`,
+      { opacity: 0 },
+      { opacity: 1, duration: 1, stagger: 0.2, ease: 'power2.inOut' }
+    )
+  }, [selectedProjectIndex])
+
+  const currentProject = myProjects[selectedProjectIndex]
+
   return (
     <section className='c-space my-20'>
-      <p className='head-text'>My Work</p>
+      <p className='head-text'>My Selected Work</p>
 
       <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full'>
         <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200'>
-          <div className='absolutetop-0 right-0'>
+          <div className='absolute top-0 right-0'>
             <img
               src={currentProject.spotlight}
               alt='spotlight'
@@ -48,9 +51,9 @@ const Projects = () => {
             className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg'
             style={currentProject.logoStyle}>
             <img
+              className='w-10 h-10 shadow-sm'
               src={currentProject.logo}
               alt='logo'
-              className='w-10 h-10 shadow-sm'
             />
           </div>
 
@@ -58,6 +61,7 @@ const Projects = () => {
             <p className='text-white text-2xl font-semibold animatedText'>
               {currentProject.title}
             </p>
+
             <p className='animatedText'>{currentProject.desc}</p>
             <p className='animatedText'>{currentProject.subdesc}</p>
           </div>
@@ -70,51 +74,43 @@ const Projects = () => {
                 </div>
               ))}
             </div>
+
             <a
+              className='flex items-center gap-2 cursor-pointer text-white-600'
               href={currentProject.href}
-              target='_black'
-              rel='noreferrer'
-              className='flex items-center gap-2 cursor-pointer text-white-600'>
+              target='_blank'
+              rel='noreferrer'>
               <p>Check Live Site</p>
-              <img
-                src='/assets/arrow-up.png'
-                alt='arrow-up'
-                className='w-3 h-3'
-              />
+              <img src='/assets/arrow-up.png' alt='arrow' className='w-3 h-3' />
             </a>
           </div>
+
           <div className='flex justify-between items-center mt-7'>
             <button
               className='arrow-btn'
               onClick={() => handleNavigation('previous')}>
-              <img
-                src='/assets/left-arrow.png'
-                alt='left-arrow'
-                className='w-4 h-4'
-              />
+              <img src='/assets/left-arrow.png' alt='left arrow' />
             </button>
+
             <button
               className='arrow-btn'
               onClick={() => handleNavigation('next')}>
               <img
                 src='/assets/right-arrow.png'
-                alt='right-arrow'
+                alt='right arrow'
                 className='w-4 h-4'
               />
             </button>
           </div>
         </div>
 
-        <div className='border border-black-300 background-black-200 rounded-lg h-96 md:h-full'>
+        <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full'>
           <Canvas>
             <ambientLight intensity={Math.PI} />
             <directionalLight position={[10, 10, 5]} />
             <Center>
               <Suspense fallback={<CanvasLoader />}>
-                <group
-                  scale={1.5}
-                  position={[0, -3, 0]}
-                  rotation={[0, -0.1, 0]}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
                   <DemoComputer texture={currentProject.texture} />
                 </group>
               </Suspense>
